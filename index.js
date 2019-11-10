0302b11d0d48f592ab148a4d98f503087d25196c
Olivia Davis <olivia.davis593@gmail.com>
2019-11-10 11:36:17 -0600

    made changes to html and js files

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 $(function(){
 
-    $('#js-shopping-list-form').submit(function(event) {
-  
-      event.preventDefault();
+  $('#js-shopping-list-form').submit(function(event) {
 
-      const listItem = $('.js-shopping-list-entry').val();
-  
-      $('.js-shopping-list-entry').val('');
-  
-      $('.shopping-list').append(
-        `<li>
-          <span class="shopping-item">${listItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>`);
-    });
-  
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
+    event.preventDefault();
 
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
-      });
-  
+    const listItem = $('.js-shopping-list-entry').val();
+
+    $('.js-shopping-list-entry').val('');
+
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
   });
+
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+
+    $(this).closest('li').remove();
+  });
+
+});
 
 
 /*
@@ -42,3 +45,5 @@ Overall Things we did:
 - removed and added check upon user click 
 - deleted button requested by user upon clicking the delete button 
 */
+
+
